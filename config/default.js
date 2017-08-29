75bd184e6cad58bf5610243cd29138d7fd3a2b4d
achinthau <achintha@duosoftware.com>
2017-08-29 11:32:28 +0530

    Update default.js

--- config/default.js
+++ config/default.js
@@ -12,7 +12,7 @@ module.exports = {
         }
     },
     "Mongo": {
-        "ip": "104.236.231.11",
+        "ip": "104.236.231.111",
         "port": "27017",
         "dbname": "dvpdb",
         "password": "DuoS123",
